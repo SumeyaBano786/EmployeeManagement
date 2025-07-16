2f3108e75c97ebae5573a06d7ec477529f00cdcc
Sumeya Bano <sumeyabano99@gmail.com>
2025-07-17 01:03:46 +0530

    file changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import LoginPage from './components/LoginPage.jsx';
+import EmployeeForm from './components/EmployeeForm.jsx';
+import EmployeeList from './components/EmployeeList.jsx'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<LoginPage/>}/>
+
+  <Route path="/employees" element={<EmployeeList/> }/>
+  <Route path="/employee/add" element={<EmployeeForm/> }/>
+</Routes>
+</BrowserRouter>
+   
+   
+   </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/EmployeeForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeeForm = () => {
+  return (
+    <div>
+      <h1>employee form here</h1>
+    </div>
+  )
+}
+
+export default EmployeeForm;
